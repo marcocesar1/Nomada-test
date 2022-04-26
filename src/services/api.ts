7f6bf2c2d4b832e9ea83260bdc3fe101073af574
marco <ramirezcesarmarco@gmail.com>
2022-04-26 18:45:12 -0500

    env file

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,17 +4,20 @@ import { handleError } from '../helpers/axios-handle-errors';
 const nomadaUrl = 'https://whois.nomada.cloud/upload';
 const moviesUrl = 'https://api.themoviedb.org/3/search/person?';
 
+const nomadaKey = process.env.REACT_APP_NOMADA_KEY || '';
+const moviesKey = process.env.REACT_APP_MOVIEDB_KEY || '';
+
 export const sendFile = <T>(data: any) => 
         axios.post<T>(nomadaUrl, data, {
             headers: {
-                'Nomada': 'ZjUyNTI1OGEtYWYxMC00NzI4LTlhYTYtYTg0NDU5OTdhMjIx'
+                'Nomada': nomadaKey
             }
         }).then(resp => resp.data).catch(handleError);
 
 export const searchPerson = <T>(query: string) => 
         axios.get<T>(moviesUrl, {
             params: {
-                api_key: 'df7f7008129612db90bc077f7bf3ebbd',
+                api_key: moviesKey,
                 query,
                 language: 'es'
             }
