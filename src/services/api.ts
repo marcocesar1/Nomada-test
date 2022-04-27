dee17d08e8be23b79e740be75ed637357e777877
marco <ramirezcesarmarco@gmail.com>
2022-04-27 16:27:57 -0500

    movies url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { handleError } from '../helpers/axios-handle-errors';
 
 const nomadaUrl = 'https://whois.nomada.cloud/upload';
-const moviesUrl = 'https://api.themoviedb.org/3/search/person?';
+const moviesUrl = 'https://api.themoviedb.org/3/search/person';
 
 const nomadaKey = process.env.REACT_APP_NOMADA_KEY || '';
 const moviesKey = process.env.REACT_APP_MOVIEDB_KEY || '';
